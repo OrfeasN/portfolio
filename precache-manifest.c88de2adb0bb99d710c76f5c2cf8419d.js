1cc96205408c107eabd9badc9eb1a3e19e834956
Orfeas Nafpliotis <orfeasdn@gmail.com>
2018-11-26 19:17:32 +0200

    Updates

---
+++ precache-manifest.c88de2adb0bb99d710c76f5c2cf8419d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "8a6304357aed31ecce8f",
+    "url": "/portfolio/static/js/main.8a630435.chunk.js"
+  },
+  {
+    "revision": "69adcb571988a25b3e3e",
+    "url": "/portfolio/static/js/1.69adcb57.chunk.js"
+  },
+  {
+    "revision": "8a6304357aed31ecce8f",
+    "url": "/portfolio/static/css/main.3eafed66.chunk.css"
+  },
+  {
+    "revision": "c9582f718633cca06fa9656adace63db",
+    "url": "/portfolio/index.html"
+  }
+];
